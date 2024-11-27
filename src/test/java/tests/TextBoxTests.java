62fc1169c64d9dc3546caa10a2410a015967dfc4
timsay022 <saifitdinov.t@yandex.ru>
2024-11-28 00:55:09 +0500

    added TextBoxTests

---
+++ src/test/java/tests/TextBoxTests.java
@@ -0,0 +1,36 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+
+    @Test
+    void fillFormTest() {
+
+        open("/text-box");
+        $("#userName").setValue("Alex");
+        $("#userEmail").setValue("alex@egorov.com");
+        $("#currentAddress").setValue("Some street");
+        $("#permanentAddress").setValue("Another street");
+        $("#submit").click();
+
+        $("#output #name").shouldHave(text("Alex"));
+        $("#output #email").shouldHave(text("alex@egorov.com"));
+        $("#output #currentAddress").shouldHave(text("Some street"));
+        $("#output #permanentAddress").shouldHave(text("Another street"));
+
+    }
+}
