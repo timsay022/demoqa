986487d0a940cbe1ce035391cc48225c8501ae94
timsay022 <saifitdinov.t@yandex.ru>
2025-01-22 19:16:33 +0500

    added selenide listener

--- src/test/java/tests/RegistrationWithPageObjectsTests.java
+++ src/test/java/tests/RegistrationWithPageObjectsTests.java
@@ -1,6 +1,9 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
@@ -37,6 +40,7 @@ public class RegistrationWithPageObjectsTests {
         Configuration.pageLoadStrategy = "eager";
         Configuration.browserSize = "1920x1080";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
     }
 
